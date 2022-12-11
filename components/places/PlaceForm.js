fdc5f743c00763af4685c872fc409a31e884fd61
schmeb0704 <seanauditor0704@gmail.com>
2022-12-11 07:18:33 -0500

    add more basic styling

--- components/places/PlaceForm.js
+++ components/places/PlaceForm.js
@@ -1,9 +1,45 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
+import { Colors } from "../../constants/colors";
 
 export default function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
+
   return (
-    <View>
-      <Text>The Place Form</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary700,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomWidth: 2,
+    borderBottomColor: Colors.primary700,
+    backgroundColor: Colors.primary100,
+  },
+});
